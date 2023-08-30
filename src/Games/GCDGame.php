0fd4d09d5bfd22cf09488e373566af7dc962d6fd
alexx <alexx13412@yandex.ru>
2023-08-30 10:54:13 +0300

    fix(checked lesson 9): fix gcd-game

--- src/Games/GCDGame.php
+++ src/Games/GCDGame.php
@@ -19,13 +19,15 @@ function run(): ?array
 
 function getGCD(int $a, int $b): int
 {
+    $a=abs($a);
+    $b=abs($b);
     if ($a < $b) {
         $tmp = $a;
         $a = $b;
         $b = $tmp;
     }
-    if ($b === 0 || $a === 0) {
-        return 0;
+    if ($b === 0) {
+        return $a;
     }
     while ($a % $b !== 0) {
         $r = $a % $b;
